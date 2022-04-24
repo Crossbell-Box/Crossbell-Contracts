28d7f74c364bfb601d9d09c67dfb950121acfabb
Albert <albert@rss3.io>
2022-04-24 10:21:10 +0800

    feat: add upgrade script

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -14,15 +14,8 @@ async function main() {
     // await hre.run('compile');
 
     const [owner, addr1] = await ethers.getSigners();
-    const admin = owner.address;
 
     // We get the contract to deploy
-
-    const MintNFT = await ethers.getContractFactory("MintNFT");
-    const mintNFT = await MintNFT.deploy();
-
-    const Linklist = await ethers.getContractFactory("Linklist");
-    const linkList = await Linklist.deploy();
 
     const InteractionLogic = await ethers.getContractFactory("InteractionLogic");
     const interactionLogic = await InteractionLogic.deploy();
@@ -41,34 +34,17 @@ async function main() {
         },
     });
     const web3Entry = await Web3Entry.deploy();
-
-    const Proxy = await ethers.getContractFactory("TransparentUpgradeableProxy");
-    const proxyWeb3Entry = await Proxy.deploy(web3Entry.address, admin, "0x");
-    const proxyLinklist = await Proxy.deploy(linkList.address, admin, "0x");
-
-    await mintNFT.deployed();
-    await linkList.deployed();
     await web3Entry.deployed();
-    await proxyWeb3Entry.deployed();
-    await proxyLinklist.deployed();
 
-    await web3Entry
-        .attach(proxyWeb3Entry.address)
-        .connect(addr1)
-        .initialize("Web3 Entry Profile", "WEP", proxyLinklist.address, mintNFT.address);
-
-    await linkList
-        .attach(proxyLinklist.address)
-        .connect(addr1)
-        .initialize("Link List Token", "LLT", proxyWeb3Entry.address);
+    // proxyWeb3Entry
+    const Proxy = await ethers.getContractFactory("TransparentUpgradeableProxy");
+    const proxyWeb3Entry = await Proxy.attach("0x72E0Bc50e94804bC8F6DDdC527c7223D3b352b43");
+    await proxyWeb3Entry.upgradeTo(web3Entry.address);
 
     console.log("InteractionLogic deployed to:", interactionLogic.address);
     console.log("ProfileLogic deployed to:", profileLogic.address);
     console.log("PostLogic deployed to:", postLogic.address);
-    console.log("Linklist deployed to:", linkList.address);
     console.log("Web3Entry deployed to:", web3Entry.address);
-    console.log("ProxyWeb3Entry deployed to:", proxyWeb3Entry.address);
-    console.log("ProxyLinklist deployed to:", proxyLinklist.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
