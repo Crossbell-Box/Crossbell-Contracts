78bc912c1efcd9500f191efa01d98cc3a9add938
Albert <albert@rss3.io>
2022-05-27 08:39:28 +0800

    chore: abi and deploy script

---
+++ scripts/deploy_resolver.ts
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+    // Hardhat always runs the compile task when running scripts with its command
+    // line interface.
+    //
+    // If this script is run directly using `node` you may want to call compile
+    // manually to make sure everything is compiled
+    // await hre.run('compile');
+
+    const [owner, addr1] = await ethers.getSigners();
+    const admin = owner.address;
+
+    const Resolver = await ethers.getContractFactory("Resolver");
+    const resolver = await Resolver.deploy();
+
+    console.log("resolver deployed to:", resolver.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
