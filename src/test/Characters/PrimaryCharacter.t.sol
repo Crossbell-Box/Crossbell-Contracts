ee561b6951ce05173655750acda85345584605b6
Fooooooooooox <crunchy.wafer.stick787@gmail.com>
2022-07-14 01:10:43 +0800

    Add: test receive character & burn

--- src/test/Characters/PrimaryCharacter.t.sol
+++ src/test/Characters/PrimaryCharacter.t.sol
@@ -8,7 +8,7 @@ import "../helpers/Const.sol";
 import "../helpers/utils.sol";
 import "../helpers/SetUp.sol";
 
-contract setUpTest is Test,Utils, SetUp {
+contract setUpTest is Test, Utils, SetUp {
     address public alice = address(0x1111);
     address public bob = address(0x2222);
     address public userThree = address(0x3333);
@@ -35,10 +35,7 @@ contract setUpTest is Test,Utils, SetUp {
         web3Entry.setPrimaryCharacterId(Const.FIRST_CHARACTER_ID);
 
         // User should set the new primary character
-        DataTypes.CreateCharacterData memory characterData2 = makeCharacterData(
-            "handle2",
-            bob
-        );
+        DataTypes.CreateCharacterData memory characterData2 = makeCharacterData("handle2", bob);
         vm.prank(bob);
         web3Entry.createCharacter(characterData2);
         vm.prank(bob);
@@ -60,7 +57,7 @@ contract setUpTest is Test,Utils, SetUp {
         web3Entry.transferFrom(bob, alice, Const.FIRST_NOTE_ID);
         uint256 primaryCharacter4 = web3Entry.getPrimaryCharacterId(bob);
         address operator2 = web3Entry.getOperator(Const.FIRST_NOTE_ID);
-        assertEq(primaryCharacter4,0);
+        assertEq(primaryCharacter4, 0);
         assertEq(operator2, address(0));
         vm.stopPrank();
     }
@@ -80,7 +77,12 @@ contract setUpTest is Test,Utils, SetUp {
         web3Entry.createCharacter(characterDataCarol2);
         // link character
         bytes memory data = new bytes(0);
-        DataTypes.linkCharacterData memory linkCharacterData = DataTypes.linkCharacterData(1,2,Const.FollowLinkType,data);
+        DataTypes.linkCharacterData memory linkCharacterData = DataTypes.linkCharacterData(
+            1,
+            2,
+            Const.FollowLinkType,
+            data
+        );
         web3Entry.linkCharacter(linkCharacterData);
         uint256 linklistid = web3Entry.getLinklistId(1, Const.FollowLinkType);
         assertEq(linklistid, Const.FIRST_LINKLIST_ID);
@@ -88,9 +90,35 @@ contract setUpTest is Test,Utils, SetUp {
         web3Entry.transferFrom(bob, alice, Const.FIRST_CHARACTER_ID);
         address CharacterOwner = web3Entry.ownerOf(1);
         address linklistOwner = linklist.ownerOf(1);
-        assertEq(CharacterOwner,alice);
-        assertEq(linklistOwner,alice);
+        assertEq(CharacterOwner, alice);
+        assertEq(linklistOwner, alice);
+
+        // User without a character, and then receives a character, it should be unset
+        web3Entry.transferFrom(bob, userThree, 2);
+        uint256 primaryCharacterUserThree = web3Entry.getPrimaryCharacterId(userThree);
+        assertEq(primaryCharacterUserThree, 0);
+        vm.stopPrank();
+
+        // userThree set primary character
+        vm.startPrank(userThree);
+        web3Entry.setPrimaryCharacterId(2);
+        uint256 primaryCharacterUserThree2 = web3Entry.getPrimaryCharacterId(userThree);
+        assertEq(primaryCharacterUserThree2, 2);
+
+        // UserTwo should fail to set handle as a character owned by user 1
+        vm.expectRevert(abi.encodePacked("NotCharacterOwner"));
+        web3Entry.setPrimaryCharacterId(1);
+
+        //UserTwo should burn primary character
+        web3Entry.burn(2);
+        uint256 userCharacterID = web3Entry.getPrimaryCharacterId(userThree);
+        assertEq(userCharacterID, 0);
+        string memory userHandel = web3Entry.getHandle(2);
+        assertEq(userHandel, "");
+        address userOperator = web3Entry.getOperator(2);
+        assertEq(userOperator, address(0));
+        DataTypes.Character memory userCharacter = web3Entry.getCharacter(2);
+        assertEq(userCharacter.noteCount, 0);
+        assertEq(userCharacter.characterId, 0);
     }
-
-
 }
