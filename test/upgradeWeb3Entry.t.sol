78b663d4c73c26e48afe3bc7ca5eb9cf80857664
Fooooooooooox <crunchy.wafer.stick787@gmail.com>
2022-11-05 16:56:45 +0800

    test: slot

--- test/upgradeWeb3Entry.t.sol
+++ test/upgradeWeb3Entry.t.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.10;
 
 import "forge-std/Test.sol";
-import "forge-std/console2.sol";
+// import "forge-std/console2.sol";
 import "../contracts/Linklist.sol";
 import "../contracts/MintNFT.sol";
 import "../contracts/Resolver.sol";
@@ -26,16 +26,21 @@ contract UpgradeWeb3Entry is Test, Utils {
     address public bob = address(0x2222);
     address public carol = address(0x3333);
 
+    address public linkList = address(0x111);
+    address public periphery = address(0x222);
+    address public mintNFT = address(0x333);
+    address public resolver = address(0x444);
+
     function setUp() public {
         web3EntryBaseImpl = new Web3EntryBase();
         proxyWeb3Entry = new TransparentUpgradeableProxy(address(web3EntryBaseImpl), admin, "");
         Web3EntryBase(address(proxyWeb3Entry)).initialize(
             Const.WEB3_ENTRY_NFT_NAME,
             Const.WEB3_ENTRY_NFT_SYMBOL,
-            address(0x111), // linklistContract
-            address(0x222), // mintNFTImpl
-            address(0x333), // periphery
-            address(0x444) // resolver
+            linkList,
+            mintNFT,
+            periphery,
+            resolver
         );
     }
 
@@ -90,12 +95,39 @@ contract UpgradeWeb3Entry is Test, Utils {
         Web3EntryBase(address(proxyWeb3Entry)).createCharacter(
             makeCharacterData(Const.MOCK_CHARACTER_HANDLE, alice)
         );
+        // set bob as operator of alice
+        vm.prank(alice);
+        Web3EntryBase(address(proxyWeb3Entry)).setOperator(Const.FIRST_CHARACTER_ID, bob);
 
+        // slot 21 is periphery address
+        bytes32 peripheryBytes32 = bytes32(0x0000000000000000000000000000000000000000000000000000000000000222);
+        bytes32 valueAt21 = vm.load(address(proxyWeb3Entry), bytes32(uint256(21)));
+        assertEq(peripheryBytes32, valueAt21);
+
+        // slot 22 is _operatorByCharacter mapping, so it's 0
+        bytes32 operatorByCharacterBytes32 = bytes32(0x0000000000000000000000000000000000000000000000000000000000000000);
+        bytes32 valueAt22 = vm.load(address(proxyWeb3Entry), bytes32(uint256(22)));
+        assertEq(operatorByCharacterBytes32, valueAt22);
+
+        // check _operatorByCharacter slot
+        bytes32 bobBytes32 = bytes32(0x0000000000000000000000000000000000000000000000000000000000002222);
+        uint256 slot = 22;
+        bytes32 operatorSlot = keccak256(abi.encodePacked(Const.FIRST_CHARACTER_ID, slot));
+        bytes32 valueAtOperatorSlot = vm.load(address(proxyWeb3Entry), operatorSlot);
+        assertEq(valueAtOperatorSlot, bobBytes32);
+
+        // slot 23 is resolver address
+        bytes32 resolverBytes32 = bytes32(0x0000000000000000000000000000000000000000000000000000000000000444);
+        bytes32 valueAt23 = vm.load(address(proxyWeb3Entry), bytes32(uint256(23)));
+        assertEq(resolverBytes32, valueAt23);
+
+        // get storage slot before the upgrade
         uint256[] memory prevSlotArr = new uint256[](25);
         for (uint256 index = 0; index < 25; index++) {
             bytes32 value = vm.load(address(proxyWeb3Entry), bytes32(uint256(index)));
             prevSlotArr[index] = uint256(value);
         }
+
 
         // upgrade to new web3Entry
         vm.startPrank(admin);
