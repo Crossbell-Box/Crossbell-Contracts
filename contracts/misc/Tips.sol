2c84e78be9d4603616d1423e8e6c6722ac1bceb7
Albert <albert@rss3.io>
2023-02-03 14:32:13 +0800

    feat: remove unused import

--- contracts/misc/Tips.sol
+++ contracts/misc/Tips.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
