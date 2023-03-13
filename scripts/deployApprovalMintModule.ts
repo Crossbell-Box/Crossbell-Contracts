934be3aeb0711e3da690a5bc8533d2ababfbd0d2
Albert <albert@rss3.io>
2023-03-13 18:53:55 +0800

    feat: add deploy script and abi

--- scripts/deployApprovalMintModule.ts
+++ scripts/deployApprovalMintModule.ts
@@ -3,15 +3,15 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from 'hardhat';
+import { ethers } from "hardhat";
 
 async function main() {
-    const proxyWeb3Entry = '0xa6f969045641Cf486a747A2688F3a5A6d43cd0D8';
+    const proxyWeb3Entry = "0xa6f969045641Cf486a747A2688F3a5A6d43cd0D8";
 
-    const ApprovalMintModule = await ethers.getContractFactory('ApprovalMintModule');
+    const ApprovalMintModule = await ethers.getContractFactory("ApprovalMintModule");
     const approvalMintModule = await ApprovalMintModule.deploy(proxyWeb3Entry);
 
-    console.log('approvalMintModule deployed to:', approvalMintModule.address);
+    console.log("approvalMintModule deployed to:", approvalMintModule.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
