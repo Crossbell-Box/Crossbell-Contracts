fdd3f1d8375fd348e8e0b14247d951a2d25ffa5c
Albert <albert@rss3.io>
2023-03-13 20:45:15 +0800

    docs: update the mint nft impl

--- contracts/MintNFT.sol
+++ contracts/MintNFT.sol
@@ -7,45 +7,67 @@ import {IMintNFT} from "./interfaces/IMintNFT.sol";
 import {IWeb3Entry} from "./interfaces/IWeb3Entry.sol";
 import {ErrCallerNotWeb3Entry} from "./libraries/Error.sol";
 import {Events} from "./libraries/Events.sol";
-import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import {IERC721Metadata} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import {
+    IERC721Enumerable
+} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
 contract MintNFT is NFTBase, IMintNFT, Initializable {
-    using Counters for Counters.Counter;
-
-    // solhint-disable var-name-mixedcase
-    address public Web3Entry;
-
     uint256 internal _characterId;
     uint256 internal _noteId;
-    Counters.Counter internal _tokenIdCounter;
+    address internal _web3Entry;
+    uint256 internal _tokenCounter;
+    mapping(uint256 => address) internal _originalReceiver;
 
+    /// @inheritdoc IMintNFT
     function initialize(
-        uint256 characterId,
-        uint256 noteId,
-        address web3Entry,
+        uint256 characterId_,
+        uint256 noteId_,
+        address web3Entry_,
         string calldata name_,
         string calldata symbol_
     ) external override initializer {
         super._initialize(name_, symbol_);
-        _characterId = characterId;
-        _noteId = noteId;
-        Web3Entry = web3Entry;
+        _characterId = characterId_;
+        _noteId = noteId_;
+        _web3Entry = web3Entry_;
 
-        emit Events.MintNFTInitialized(characterId, noteId, block.timestamp);
+        emit Events.MintNFTInitialized(characterId_, noteId_, block.timestamp);
     }
 
-    function mint(address to) external override returns (uint256) {
-        if (msg.sender != Web3Entry) revert ErrCallerNotWeb3Entry();
+    /// @inheritdoc IMintNFT
+    function mint(address to) external override returns (uint256 tokenId) {
+        if (msg.sender != _web3Entry) revert ErrCallerNotWeb3Entry();
 
-        _tokenIdCounter.increment();
-        _mint(to, _tokenIdCounter.current());
-        return _tokenIdCounter.current();
+        tokenId = ++_tokenCounter;
+        _mint(to, tokenId);
     }
 
+    /// @inheritdoc IMintNFT
+    function originalReceiver(uint256 tokenId) external view override returns (address) {
+        return _originalReceiver[tokenId];
+    }
+
+    /// @inheritdoc IMintNFT
+    function getSourceNotePointer()
+        external
+        view
+        override
+        returns (uint256 characterId, uint256 noteId)
+    {
+        return (_characterId, _noteId);
+    }
+
+    /// @inheritdoc IERC721Enumerable
+    function totalSupply() public view override returns (uint256) {
+        return _tokenCounter;
+    }
+
+    /// @inheritdoc IERC721Metadata
     function tokenURI(uint256 tokenId) public view override returns (string memory uri) {
         if (_exists(tokenId)) {
-            uri = IWeb3Entry(Web3Entry).getNote(_characterId, _noteId).contentUri;
+            uri = IWeb3Entry(_web3Entry).getNote(_characterId, _noteId).contentUri;
         }
     }
 }
--- contracts/interfaces/IMintNFT.sol
+++ contracts/interfaces/IMintNFT.sol
@@ -3,13 +3,40 @@
 pragma solidity 0.8.16;
 
 interface IMintNFT {
+    /**
+     * @notice Initialize the mint nft.
+     * @param characterId_  The character ID of the note to initialize.
+     * @param noteId_  The note ID to initialize.
+     * @param web3Entry_  The address of web3Entry contract.
+     * @param name_ The name to set for this NFT.
+     * @param symbol_ The symbol to set for this NFT.
+     */
     function initialize(
-        uint256 characterId,
-        uint256 noteId,
-        address web3Entry,
-        string calldata name,
-        string calldata symbol
+        uint256 characterId_,
+        uint256 noteId_,
+        address web3Entry_,
+        string calldata name_,
+        string calldata symbol_
     ) external;
 
+    /**
+     * @notice Mints a note NFT to the specified address.
+     * This can only be called by web3Entry, and is called upon note.
+     * @param to The address to mint the NFT to.
+     * @return uint256 The minted token ID.
+     */
     function mint(address to) external returns (uint256);
+
+    /**
+     * @notice Returns the original receiver of specified NFT.
+     * @return The address of original receiver.
+     */
+    function originalReceiver(uint256 tokenId) external view returns (address);
+
+    /**
+     * @notice Returns the source note pointer mapped to this note NFT.
+     * @return characterId The character ID.
+     * @return noteId The note ID.
+     */
+    function getSourceNotePointer() external view returns (uint256 characterId, uint256 noteId);
 }
