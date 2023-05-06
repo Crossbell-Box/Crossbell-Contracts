e86a5f7ad3b47fe96b9b96a09bb752c44a8bb499
Albert <albert@rss3.io>
2023-05-06 15:14:18 +0800

    chore: update abi and deploy script

--- scripts/deployNewbieVilla.ts
+++ scripts/deployNewbieVilla.ts
@@ -17,10 +17,11 @@ async function main() {
     const proxyWeb3Entry = "0xa6f969045641Cf486a747A2688F3a5A6d43cd0D8";
     const xsyncOperator = "0x0F588318A494e4508A121a32B6670b5494Ca3357";
     const miraToken = "0xAfB95CC0BD320648B3E8Df6223d9CDD05EbeDC64";
+    const tips = "0x0058be0845952D887D1668B5545de995E12e8783";
 
     const NewbieVilla = await ethers.getContractFactory("NewbieVilla");
     const newbieVilla = await NewbieVilla.deploy();
-    await newbieVilla.initialize(proxyWeb3Entry, xsyncOperator, miraToken);
+    await newbieVilla.initialize(proxyWeb3Entry, xsyncOperator, miraToken, tips);
 
     console.log("newbieVilla deployed to:", newbieVilla.address);
 }
